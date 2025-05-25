973a8bfa8e9da9f56bbc74277f1b724761f163ac
shimizu <noon3m.op.sh@gmail.com>
2025-05-25 13:06:43 +0900

    [wip] 画像分割のテストコード

--- front/src/pages/camera.tsx
+++ front/src/pages/camera.tsx
@@ -1,19 +1,21 @@
 //撮影画面
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
+import  CameraFrame  from "./face_crop";
 
 function Camera() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const gotoChange = () => {
-        navigate("/photo_change");
-    }
+  const gotoChange = () => {
+    navigate("/photo_change");
+  };
 
-    return(
-        <>
-        <h1>カメラぱしゃ</h1>
-        <button onClick={gotoChange}>スーツチェンジ</button>
-        </>
-    )
+  return (
+    <>
+      <h1>カメラぱしゃ</h1>
+      <button onClick={gotoChange}>スーツチェンジ</button>
+      <CameraFrame />
+    </>
+  );
 }
 
 export default Camera;
